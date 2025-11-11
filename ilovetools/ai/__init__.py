9791ffe6cac3b40bb30e200c6e3ca409915724ab
Ali Mehdi <105543812+AliMehdi512@users.noreply.github.com>
2025-11-11 20:34:50 +0500

    Create ai module structure

---
+++ ilovetools/ai/__init__.py
@@ -0,0 +1,11 @@
+"""
+AI & Machine Learning utilities module
+"""
+
+from .llm_helpers import *
+from .embeddings import *
+from .inference import *
+
+__all__ = [
+    # Will be populated as functions are added
+]
