1deb38f6bb121e309b811ea05b46bc1a6a9af231
Ali Mehdi <105543812+AliMehdi512@users.noreply.github.com>
2025-11-21 11:47:51 +0500

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ilovetools",
-    version="0.1.5",
+    version="0.1.6",
     author="Ali Mehdi",
     author_email="ali.mehdi.dev579@gmail.com",
     description="A comprehensive Python utility library with modular tools for AI/ML, data processing, and daily programming needs",
