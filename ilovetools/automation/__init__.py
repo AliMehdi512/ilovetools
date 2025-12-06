f0b75c4fcc12762cb894a8e719acdc79c2c9b87a
Ali Mehdi <105543812+AliMehdi512@users.noreply.github.com>
2025-12-06 16:44:10 +0500

    Update automation module exports

--- ilovetools/automation/__init__.py
+++ ilovetools/automation/__init__.py
@@ -2,4 +2,24 @@
 Task automation utilities
 """
 
-__all__ = []
+from .file_organizer import (
+    organize_ext,
+    organize_date,
+    organize_size,
+    organize_pattern,
+    scan_directory,
+    move_files_safely,
+    create_folder_structure,
+    undo_organization,
+)
+
+__all__ = [
+    'organize_ext',
+    'organize_date',
+    'organize_size',
+    'organize_pattern',
+    'scan_directory',
+    'move_files_safely',
+    'create_folder_structure',
+    'undo_organization',
+]
