4fbdec3b63e5ef3ef79a80a59c60a737d1f255b0
Ali Mehdi <105543812+AliMehdi512@users.noreply.github.com>
2025-12-06 16:43:11 +0500

    Update web module exports with new tools

--- ilovetools/web/__init__.py
+++ ilovetools/web/__init__.py
@@ -2,4 +2,49 @@
 Web scraping and HTTP utilities
 """
 
-__all__ = []
+from .scraper import (
+    extract_links,
+    extract_emails,
+    extract_phone_numbers,
+    extract_images,
+    extract_text,
+    extract_metadata,
+    extract_tables,
+    extract_json_ld,
+    rate_limiter,
+    parse_html_simple,
+    clean_text,
+    get_domain,
+    is_valid_url,
+)
+
+from .url_shortener import (
+    URLShortener,
+    InMemoryStorage,
+    ShortURL,
+    generate_short_code,
+    validate_custom_alias,
+)
+
+__all__ = [
+    # Scraper
+    'extract_links',
+    'extract_emails',
+    'extract_phone_numbers',
+    'extract_images',
+    'extract_text',
+    'extract_metadata',
+    'extract_tables',
+    'extract_json_ld',
+    'rate_limiter',
+    'parse_html_simple',
+    'clean_text',
+    'get_domain',
+    'is_valid_url',
+    # URL Shortener
+    'URLShortener',
+    'InMemoryStorage',
+    'ShortURL',
+    'generate_short_code',
+    'validate_custom_alias',
+]
