7205aca010ada37f5567403f2d7906dc0676eeb6
AliMehdi512 <f2021266579@umt.edu.pk>
2025-12-10 16:46:29 +0500

    chore(release): bump version to 0.2.9.post2

--- ilovetools/__init__.py
+++ ilovetools/__init__.py
@@ -2,7 +2,7 @@
 ilovetools - A comprehensive Python utility library
 """
 
-__version__ = "0.2.9.post1"
+__version__ = "0.2.9.post2"
 __author__ = "Ali Mehdi"
 __email__ = "ali.mehdi.dev579@gmail.com"
 
