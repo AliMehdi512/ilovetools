b13c44ef952e0907c5acfaa188e8b795c391ad7b
Ali Mehdi <105543812+AliMehdi512@users.noreply.github.com>
2025-12-16 12:19:37 +0500

    Update __init__.py version to 0.2.7.1

--- ilovetools/__init__.py
+++ ilovetools/__init__.py
@@ -2,8 +2,8 @@
 ilovetools - A comprehensive Python utility library
 """
 
-__version__ = "0.2.11"
-# release marker: 0.2.11
+__version__ = "0.2.7.1"
+# release marker: 0.2.7.1
 __author__ = "Ali Mehdi"
 __email__ = "ali.mehdi.dev579@gmail.com"
 
